0847ac48f45df0d6afbf5cb9a5dfa32528ba9843
Sriharish Koka <skoka@appnexus.com>
2018-04-30 22:48:55 -0700

    Basic qwerty

--- keyboards/lightcycle/keymaps/qwerty/keymap.c
+++ keyboards/lightcycle/keymaps/qwerty/keymap.c
@@ -39,18 +39,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = LAYOUT_lightcycle(  // layer 0 : default
         // left hand
-          KC_ESC,       KC_QUOT,   KC_COMM,  KC_DOT,   KC_P,    KC_Y,                     
-          KC_TAB,       KC_A,      KC_O,     KC_E,     KC_U,    KC_I,                     
-          KC_LSFT,      KC_SCLN,   KC_Q,     KC_J,     KC_K,    KC_X,                     
-          KC_LCTL,      KC_LGUI,  KC_LALT,  KC_LEFT,  KC_RGHT,                           
-                                                                      KC_ENT,  KC_PGUP,  
-                                                             KC_BSPC, MO(NUMB), KC_PGDN,
+          KC_ESC,       KC_Q,      KC_W,     KC_E,     KC_R,    KC_T,                     
+          KC_TAB,       KC_A,      KC_S,     KC_D,     KC_F,    KC_G,                     
+          KC_LSFT,      KC_Z,      KC_X,     KC_C,     KC_V,    KC_B,                     
+          KC_LCTL,      KC_LGUI,   KC_LALT,  KC_LEFT,  KC_RGHT,                           
+                                                                        KC_ENT,   KC_PGUP,  
+                                                               KC_BSPC, MO(NUMB), KC_PGDN,
                                                                                           
         // right hand                                                                     
-                  KC_F,         KC_G,     KC_C,     KC_R,     KC_L,     KC_SLSH,          
-                  KC_D,         KC_H,     KC_T,     KC_N,     KC_S,     KC_MINS,          
-                  KC_B,         KC_M,     KC_W,     KC_V,     KC_Z,     KC_RSFT,          
-                                KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,  KC_RALT,          
+                            KC_Y,   KC_U,     KC_I,     KC_O,     KC_P,        KC_SLSH,          
+                            KC_H,   KC_J,     KC_K,     KC_L,     KC_SCLN,     KC_MINS,          
+                            KC_N,   KC_M,     KC_W,     KC_V,     KC_Z,        KC_RSFT,          
+                                    KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,     KC_RALT,          
         KC_HOME,  KC_DEL,                                                                 
         KC_END,   MO(SYMB), KC_SPC                                                          
     ),
