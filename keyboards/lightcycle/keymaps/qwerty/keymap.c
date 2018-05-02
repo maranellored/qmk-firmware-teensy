ef48cff39c5421b57a035ab32a2328cc92cbec23
Sriharish Koka <skoka@appnexus.com>
2018-05-01 18:51:51 -0700

    Move enter around

--- keyboards/lightcycle/keymaps/qwerty/keymap.c
+++ keyboards/lightcycle/keymaps/qwerty/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             KC_N,   KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,     KC_RBRC,
                                     KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,     KC_RALT,
         MO(SYMB),  KC_DEL,
-        KC_RSFT,   KC_RGUI, KC_SPC 
+        KC_ENT,    KC_RGUI, KC_SPC 
     ),
 
 /* Keymap 1: Number Layer
